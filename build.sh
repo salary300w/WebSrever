da8cbdf49d26a31775e91f869cf7ac35204b3ce5
tengxunyun <tengxunyun>
2024-04-06 16:02:22 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-make server
+# 检查是否存在 build 文件夹
+if [ -d "build" ]; then
+    echo "Deleting existing build directory..."
+    rm -rf build  # 删除 build 文件夹及其内容
+    echo "Build directory deleted."
+else
+    echo "Build directory does not exist."
+fi
+mkdir build
+echo "make build directory..."
+
+cd build
+
+cmake ..
+ 
+make -j
+
