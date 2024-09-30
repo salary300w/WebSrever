8cea830fe0e37925129bffd52eb22e0b948bf56b
huaweiyun <huaweiyun@huawei.com>
2024-10-01 02:48:52 +0800

    展示不同选股策略的数据

---
+++ root/StrategyB/articles.js
@@ -0,0 +1,4 @@
+const articles = [,
+    { title: "2024_10_16", file: "2024_10_16.html", summary: "2024_10_16股票数据"},
+    { title: "2024_10_15", file: "2024_10_15.html", summary: "2024_10_15股票数据"},
+];
--- root/articles.js
+++ root/articles.js
@@ -1,8 +1,4 @@
 const articles = [,
-    { title: "2024_09_27", file: "2024_09_27.html", summary: "2024_09_27股票数据"},
-    { title: "2024_09_26", file: "2024_09_26.html", summary: "2024_09_26股票数据"},
-    { title: "2024_09_25", file: "2024_09_25.html", summary: "2024_09_25股票数据"},
-    { title: "2024_09_24", file: "2024_09_24.html", summary: "2024_09_24股票数据"},
-    { title: "2024_09_23", file: "2024_09_23.html", summary: "2024_09_23股票数据"},
-    { title: "2024_09_22", file: "2024_09_22.html", summary: "2024_09_22股票数据"},
+    { title: "StrategyA", file: "StrategyA/PageMasterStrategyA.html", summary: "选股策略A"},
+    { title: "StrategyB", file: "StrategyB/PageMasterStrategyB.html", summary: "选股策略B"},
 ];
