fd00a871c5c46de54bde3ff04cdb4e4facaa72c9
huaweiyun <huaweiyun@huawei.com>
2024-10-17 21:20:05 +0800

    1

--- root/StrategyA/articles.js
+++ root/StrategyA/articles.js
@@ -1,4 +1,5 @@
 const articles = [,
+    { title: "2024_10_17", file: "2024_10_17.html", summary: "2024_10_17股票数据"},
     { title: "2024_10_16", file: "2024_10_16.html", summary: "2024_10_16股票数据"},
     { title: "2024_10_15", file: "2024_10_15.html", summary: "2024_10_15股票数据"},
     { title: "2024_10_14", file: "2024_10_14.html", summary: "2024_10_14股票数据"},
